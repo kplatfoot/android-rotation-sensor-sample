1c0d56d3d0253d39ec4f4e0567dcb25f85340b50
Keith Platfoot <kplatfoot@gmail.com>
2017-11-07 19:56:58 -0500

    Upgrade to Architecture Components 1.0.
    Also use Java 8 for lambda syntax.

--- app/src/main/java/com/kviation/sample/orientation/MainActivity.java
+++ app/src/main/java/com/kviation/sample/orientation/MainActivity.java
@@ -1,32 +1,18 @@
 package com.kviation.sample.orientation;
 
-import android.arch.lifecycle.LifecycleRegistry;
-import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements LifecycleRegistryOwner {
-
-  private LifecycleRegistry mLifecycleRegistry = new LifecycleRegistry(this);
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    final AttitudeIndicator aiView = (AttitudeIndicator) findViewById(R.id.attitude_indicator);
+    AttitudeIndicator aiView = findViewById(R.id.attitude_indicator);
 
-    Orientation orientation = new Orientation(this, new Orientation.Listener() {
-      @Override
-      public void onOrientationChanged(float pitch, float roll) {
-        aiView.setAttitude(pitch, roll);
-      }
-    });
-    getLifecycle().addObserver(orientation);
-  }
-
-  @Override
-  public LifecycleRegistry getLifecycle() {
-    return mLifecycleRegistry;
+    getLifecycle().addObserver(new Orientation(this,
+            (pitch, roll) -> aiView.setAttitude(pitch, roll)));
   }
 }
